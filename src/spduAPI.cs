1fcbbf5e1fa27e8a3c83a90acfd9913a4c1c8588
hy6p <roman.tata.b@gmail.com>
2021-02-26 09:24:16 -0800

    code cleanup

--- src/spduAPI.cs
+++ src/spduAPI.cs
@@ -3,7 +3,6 @@ using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -24,10 +23,6 @@ namespace API
             [Blob("test", Connection = "AzureWebJobsStorage")] CloudBlobContainer storageBlob,
             ILogger _log)
         {
-            // if (!req.IsHttps)
-            // {
-            //     return new BadRequestResult();
-            // }
 
             IActionResult result;
             log = _log;
@@ -57,7 +52,6 @@ namespace API
             using (Aes cypher = Aes.Create())
             {
                 string key = "abcdefghijklmnop";
-                // byte[] buf = Encoding.ASCII.GetBytes(requestBody);
                 byte[] buf = reqBody;
                 string decrypted = DecryptStringFromBytes_Aes(buf, Encoding.ASCII.GetBytes(key), Encoding.ASCII.GetBytes(key));
                 log.LogInformation(decrypted);
@@ -102,73 +96,30 @@ namespace API
             using (Aes aesAlg = Aes.Create())
             {
                 aesAlg.Clear();
-                // aesAlg.Padding = PaddingMode.None;
                 aesAlg.Padding = PaddingMode.PKCS7;
-                // aesAlg.Mode = CipherMode.CBC;
                 aesAlg.BlockSize = 128;
                 aesAlg.Key = Key;
                 aesAlg.IV = IV;
-                // aesAlg.Padding = PaddingMode.Zeros;
 
                 log.LogInformation(Encoding.ASCII.GetString(aesAlg.Key));
                 log.LogInformation(Encoding.ASCII.GetString(aesAlg.IV));
-                // Create a decryptor to perform the stream transform.
                 ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
                 // Create the streams used for decryption.
                 using (MemoryStream msDecrypt = new MemoryStream(cipherText))
                 {
                     byte[] pre_cipher = msDecrypt.ToArray();
-                    // msDecrypt.
-                    // plaintext = Encoding.Default.GetString(plainTextArr)
                     log.LogInformation("pre cipher length - {0}", pre_cipher.Length);
                     log.LogInformation(Encoding.ASCII.GetString(pre_cipher));
 
                     using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                     {
-                        // try {
-                        //     csDecrypt.Write(cipherText);
-                        // }
-                        // catch (Exception e) {
-                        //     log.LogInformation(e.ToString());
-                        // }
-                        // csDecrypt.
-                        // using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                        // {
-
-                        //     // Read the decrypted bytes from the decrypting stream
-                        //     // and place them in a string.
-                        //     byte[] plTxt = msDecrypt.
-                        //     plaintext = srDecrypt.ReadToEnd();
-                        // }
                         using(StreamReader srDecrypt = new StreamReader(csDecrypt)) {
                             plaintext = srDecrypt.ReadToEnd();
                             log.LogInformation(plaintext);
                         }
 
                     }
-                    // try 
-                    // {
-                    //     // msDecrypt.
-                    //     byte[] plainTextArr = msDecrypt.ToArray();
-
-                    //     for (int i = 0; i < plainTextArr.Length; i++) {
-                    //         log.LogInformation(plainTextArr[i].ToString());
-                    //         // if (plainTextArr[i] != 0) {
-                    //             // log.LogInformation("null char - {0}", 0);
-                    //             // log.LogInformation("padding ends with {0} at {1}", plainTextArr[i], i);
-                    //             // log.LogInformation("message ends with {0} at {1}", plainTextArr[i], i);
-                    //             // log.LogInformation((plainTextArr.Length - i).ToString());
-                    //             // break;
-                    //         // }
-                    //     }
-                    //     // for (int i = )
-                    //     plaintext = Encoding.ASCII.GetString(plainTextArr);
-                    // }
-                    // catch (Exception e)
-                    // {
-                    //     log.LogInformation(e.ToString());
-                    // }
                 }
             }
 
